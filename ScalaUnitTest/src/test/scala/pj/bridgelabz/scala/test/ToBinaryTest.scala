f3c2f9bd4ea090a963ef372eed1b6631b73a2fed
PiyushMauli <jiwanepiyush@gmail.com>
2021-05-19 14:24:41 +0530

    we have tested the decimal to binary conversion functionality

--- ScalaUnitTest/src/test/scala/pj/bridgelabz/scala/test/ToBinaryTest.scala
+++ ScalaUnitTest/src/test/scala/pj/bridgelabz/scala/test/ToBinaryTest.scala
@@ -1,5 +1,13 @@
 package pj.bridgelabz.scala.test
 
-class ToBinaryTest {
+import org.scalatest.flatspec.AnyFlatSpec
+import pj.bridgelabz.scala.BinaryCheck
 
+class ToBinaryTest extends AnyFlatSpec{
+  it should "match" in {
+    assert("110 1010"==new BinaryCheck(106).generateBinaryNumber())
+  }
+  it should "match2" in {
+    assert("10 1100"==new BinaryCheck(44).generateBinaryNumber())
+  }
 }
