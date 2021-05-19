962961516364994a14d5a90a072a5556f4820eec
PiyushMauli <jiwanepiyush@gmail.com>
2021-05-19 14:01:55 +0530

    added test cases to check correct day on date provided by user

--- ScalaUnitTest/src/test/scala/pj/bridgelabz/scala/test/DayOfWeekTest.scala
+++ ScalaUnitTest/src/test/scala/pj/bridgelabz/scala/test/DayOfWeekTest.scala
@@ -1,3 +1,14 @@
 package pj.bridgelabz.scala.test
 
-case class DayOfWeekTest()
+import org.scalatest.flatspec.AnyFlatSpec
+import pj.bridgelabz.scala.CalenderCheck
+
+case class DayOfWeekTest() extends AnyFlatSpec{
+  it should "match" in {
+    assert("Wednesday"==new CalenderCheck(2021,5,5).checkDay())
+  }
+
+  it should "match2" in {
+    assert("Friday"==new CalenderCheck(2006,5,5).checkDay())
+  }
+}
