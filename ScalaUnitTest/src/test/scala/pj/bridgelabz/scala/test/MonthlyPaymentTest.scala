636d4d4186941459fa9c027ff49f1c44219d7a79
PiyushMauli <jiwanepiyush@gmail.com>
2021-05-19 14:17:25 +0530

    we have successfully test the monthlu EMI value based on the values provided by the user

--- ScalaUnitTest/src/test/scala/pj/bridgelabz/scala/test/MonthlyPaymentTest.scala
+++ ScalaUnitTest/src/test/scala/pj/bridgelabz/scala/test/MonthlyPaymentTest.scala
@@ -1,5 +1,10 @@
 package pj.bridgelabz.scala.test
 
-class MonthlyPaymentTest {
+import org.scalatest.flatspec.AnyFlatSpec
+import pj.bridgelabz.scala.PaymentCheck
 
+class MonthlyPaymentTest extends AnyFlatSpec{
+  it should "match" in {
+    assert(461.44926337516637==new PaymentCheck(10000,2,10).monthlyPayment())
+  }
 }
