34272a8fa91167ab1e338805b15689377de267de
PiyushMauli <jiwanepiyush@gmail.com>
2021-05-19 13:28:35 +0530

    here we have successfully test the vending machine program and check machine return correct notes or not

--- ScalaUnitTest/src/test/scala/pj/bridgelabz/scala/test/VendingMachineTest.scala
+++ ScalaUnitTest/src/test/scala/pj/bridgelabz/scala/test/VendingMachineTest.scala
@@ -1,3 +1,13 @@
 package pj.bridgelabz.scala.test
 
-case class VendingMachineTest()
+import org.scalatest.flatspec.AnyFlatSpec
+import pj.bridgelabz.scala.CheckNotes
+
+class VendingMachineTest extends AnyFlatSpec{
+  it should "match" in {
+    assert(9==new CheckNotes(972).calculateNumberOfNotes())
+  }
+  it should "match2" in {
+    assert(19==new CheckNotes(12543).calculateNumberOfNotes())
+  }
+}
