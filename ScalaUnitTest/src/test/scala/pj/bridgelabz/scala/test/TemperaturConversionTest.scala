8ad08c9c58d66115007f56818b049251ccb42510
PiyushMauli <jiwanepiyush@gmail.com>
2021-05-19 14:10:47 +0530

    we have successfully tested the program and functionality

--- ScalaUnitTest/src/test/scala/pj/bridgelabz/scala/test/TemperaturConversionTest.scala
+++ ScalaUnitTest/src/test/scala/pj/bridgelabz/scala/test/TemperaturConversionTest.scala
@@ -1,5 +1,13 @@
 package pj.bridgelabz.scala.test
 
-class TemperaturConversionTest {
+import org.scalatest.flatspec.AnyFlatSpec
+import pj.bridgelabz.scala.TempratureCheck
 
+class TemperaturConversion extends AnyFlatSpec{
+  it should "match" in {
+    assert(122==new TempratureCheck(degreeCelsius = 50).temperaturConversionCelsius())
+  }
+  it should "match2" in {
+    assert(10==new TempratureCheck(degreeFahrenhit = 50).temperaturConversionFahrenhit())
+  }
 }
