db9834172cff3a17cdd4f0c316b9ac8f4cc5a003
PiyushMauli <jiwanepiyush@gmail.com>
2021-05-19 14:33:33 +0530

    test the functionality to read an integer as an Input, convert to Binary using toBinary function and perform the following functions like Swap nibbles and find the new number and Find the resultant number is the number is a power of 2.

--- ScalaUnitTest/src/test/scala/pj/bridgelabz/scala/test/BinaryTest.scala
+++ ScalaUnitTest/src/test/scala/pj/bridgelabz/scala/test/BinaryTest.scala
@@ -1,5 +1,13 @@
 package pj.bridgelabz.scala.test
 
-class BinaryTest {
+import org.scalatest.flatspec.AnyFlatSpec
+import pj.bridgelabz.scala.Convert
 
+class BinaryTest extends AnyFlatSpec{
+  it should "match" in {
+    assert(70.0==new Convert(100).convertBinaryToDecimal())
+  }
+  it should "match2" in {
+    assert(6.0==new Convert(96).convertBinaryToDecimal())
+  }
 }
