c2387bb5804df3ff2338c437a55c3ee55d0aca41
PiyushMauli <jiwanepiyush@gmail.com>
2021-05-26 20:50:57 +0530

    added a code to read and write the json object

--- ObjectOrientedPrograms/src/main/scala/pj/bridgelabz/scala/InventoryManager.scala
+++ ObjectOrientedPrograms/src/main/scala/pj/bridgelabz/scala/InventoryManager.scala
@@ -1,5 +1,29 @@
 package pj.bridgelabz.scala
 
-object InventoryManager {
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
+import java.io.{ByteArrayOutputStream, File}
+import java.nio.file.Paths
+
+case class Inventory(var name:String,var price:Int)
+
+//case class ImpItem(var item:Array[Inventory])
+
+case class ReadData(var item:String,var list:List[Inventory])
+
+object InventoryManager {
+  def main(args: Array[String]): Unit = {
+    val mapper:ObjectMapper=new ObjectMapper();
+    mapper.registerModule(DefaultScalaModule)
+    val root:JsonNode=mapper.readTree(new File("src/main/json/pj/bridgelabz/json/InventoryItems.json"))
+    var itemList=List(Inventory("a",500),Inventory("b",200))
+    val node1:JsonNode=mapper.valueToTree(itemList)
+    root.asInstanceOf[ObjectNode].put("TuarDaal",node1)
+    mapper.writerWithDefaultPrettyPrinter().writeValue(new File("src/main/json/pj/bridgelabz/json/InventoryItems.json"),root);
+
+
+    println(mapper.readValue(Paths.get("src/main/json/pj/bridgelabz/json/InventoryItems.json").toFile(),classOf[Map[String, Seq[Inventory]]]))
+  }
 }
