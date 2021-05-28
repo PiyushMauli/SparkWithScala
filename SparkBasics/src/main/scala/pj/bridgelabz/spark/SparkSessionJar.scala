ad41ad9e3bf310e8c0eea5a3142fd1fcd01f151e
PiyushMauli <jiwanepiyush@gmail.com>
2021-05-28 13:47:57 +0530

    we have created a SparkSession class object so that we can create a .jar file out of it and use in our new program

--- SparkBasics/src/main/scala/pj/bridgelabz/spark/SparkSessionJar.scala
+++ SparkBasics/src/main/scala/pj/bridgelabz/spark/SparkSessionJar.scala
@@ -1,5 +1,12 @@
 package pj.bridgelabz.spark
 
-object SparkSessionJar {
+import org.apache.spark.sql.SparkSession
 
+object SparkSessionJar {
+  def main(args: Array[String]): Unit = {
+    val sparkSession=SparkSession.builder()
+      .appName("Create a Spark Session")
+      .master("local[3]")
+      .getOrCreate()
+  }
 }
