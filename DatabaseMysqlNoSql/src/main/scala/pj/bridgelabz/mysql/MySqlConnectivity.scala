908dfe0f4fd24a58a802cc2b1961a1f70cde3ec5
PiyushMauli <jiwanepiyush@gmail.com>
2021-05-29 12:29:20 +0530

    in our mysql database we have a demo database and in it we have city table, in this program we are reading city names present in that table

--- DatabaseMysqlNoSql/src/main/scala/pj/bridgelabz/mysql/MySqlConnectivity.scala
+++ DatabaseMysqlNoSql/src/main/scala/pj/bridgelabz/mysql/MySqlConnectivity.scala
@@ -1,5 +1,26 @@
 package pj.bridgelabz.mysql
 
-object MySqlConnectivity {
+import java.sql.{Connection, DriverManager}
 
+object MySqlConnectivity extends App {
+    // connect to the database named "mysql" on port 8889 of localhost
+    val url = "jdbc:mysql://localhost:3306/demo"
+    val driver = "com.mysql.cj.jdbc.Driver"
+    val username = "root"
+    val password = "root"
+    var connection:Connection = _
+    try {
+      Class.forName(driver)
+      connection = DriverManager.getConnection(url, username, password)
+      val statement = connection.createStatement
+      val rs = statement.executeQuery("SELECT * FROM city")
+      while (rs.next) {
+        val host = rs.getString("cityname")
+        //      val user = rs.getString("user")
+        println("City Name = %s".format(host))
+      }
+    } catch {
+      case e: Exception => e.printStackTrace
+    }
+    connection.close
 }
