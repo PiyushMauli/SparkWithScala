ea642340ed87bbc42274767b8d9cba7b384bf2e9
PiyushMauli <jiwanepiyush@gmail.com>
2021-05-29 13:39:19 +0530

    we have a DemoDB database and inside of it demodata table in mongodb database locally. With the help of this program we are reading all the record presend insode the table

--- DatabaseMysqlNoSql/src/main/scala/pj/bridgelabz/mysql/NoSqlConnectivity.scala
+++ DatabaseMysqlNoSql/src/main/scala/pj/bridgelabz/mysql/NoSqlConnectivity.scala
@@ -1,5 +1,30 @@
 package pj.bridgelabz.mysql
 
-object NoSqlConnectivity {
+import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase, Observer}
 
+object NoSqlConnectivity {
+  def main(args: Array[String]): Unit = {
+    val mongoClient: MongoClient = MongoClient("mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb")
+    val database: MongoDatabase = mongoClient.getDatabase("DemoDB")
+
+    val collection: MongoCollection[Document] =database.getCollection("demodata");
+
+    val observable = collection.find()
+
+    observable.subscribe ( new Observer[Document] {
+      override def onNext(result: Document): Unit = println(result.toJson())
+      override def onError(e: Throwable): Unit = println("Failed" + e.getMessage)
+      override def onComplete(): Unit = println("Completed")
+    })
+
+//    println(s"observable = ${observable}")
+
+
+//
+//    observable.subscribe ( new Observer[Document] {
+//      override def onNext(result: Document): Unit = println(result.toJson())
+//      override def onError(e: Throwable): Unit = println("Failed" + e.getMessage)
+//      override def onComplete(): Unit = println("Completed")
+//    })
+  }
 }
